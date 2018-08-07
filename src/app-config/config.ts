b19c910a33fc04f3881d221ed01316b68ba3ea18
SANGSOO KIM <lonycell@gmail.com>
2018-08-07 13:22:40 +0900

    basic default configuration on app-config.

--- src/app-config/config.ts
+++ src/app-config/config.ts
@@ -1,6 +1,24 @@
 class AppConfigCtrl {
   static template = require("./config.html");
+  title: string = "Edge AI Configurator";
   appEditCtrl: any;
+  appModel: any;
+  editForm: any;
+  mqtt: any;
+  storage: any;
+
+  suggest: any = {
+    storage: {
+      base: 'edge',
+      data: 'data'
+    },
+    mqtt: {
+      brocker: 'localhost:3306',
+      base: 'THINGSPIN/EDGEAI',
+      inprefix: 'IN',
+      outprefix: 'OUT',
+    }
+  }; 
 
   constructor(private $q) {
     this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
