6d27f62afc6fc9e03407411ac1eaf75bb604d565
Gunwoo Cho <gunwoo@hancommds.com>
2018-08-21 09:41:02 +0900

    fix multi style load

--- src/monitoring/components/app.component.ts
+++ src/monitoring/components/app.component.ts
@@ -1,21 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { StateService, UIRouter } from '@uirouter/core';
 
-function getStyleUrls(): string [] {
-    let theme: string = ((<any>window).thingspinBootData.user.lightTheme) ? 'light' : 'dark';
-    let csss: string[] = [];
-
-    let prefix = 'public/plugins/proj-edge-ai-app/css/monitoring/';
-
-    csss.push(prefix + theme + '.css');
-
-    return csss;
-}
-
 @Component ({
     selector: 'edge-ai-inference-monitoring',
     template: require(`./app.component.html`),
-    styleUrls: getStyleUrls(),
 })
 export class AppComponent implements OnInit {
     title: String = 'Script Monitoring';
